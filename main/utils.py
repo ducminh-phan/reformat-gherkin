7c2d9d68db152162af0a3ce8b7b05c9a8138afd5
Duc-Minh Phan <alephvn@gmail.com>
2019-03-29 23:01:12 +0700

    Add utility functions

--- main/utils.py
+++ main/utils.py
@@ -1,4 +1,6 @@
+import difflib
 import re
+import tempfile
 
 _first_cap_re = re.compile(r"(.)([A-Z][a-z]+)")
 _all_cap_re = re.compile(r"([a-z0-9])([A-Z])")
@@ -11,3 +13,26 @@ def camel_to_snake_case(name):
     """
     s1 = _first_cap_re.sub(r"\1_\2", name)
     return _all_cap_re.sub(r"\1_\2", s1).lower()
+
+
+def dump_to_file(*output: str) -> str:
+    """
+    Dump `output` to a temporary file. Return path to the file.
+    """
+    with tempfile.NamedTemporaryFile(
+        mode="w", prefix="rfmt-ghk_", suffix=".log", delete=False, encoding="utf-8"
+    ) as f:
+        for lines in output:
+            f.write(lines)
+            if lines and lines[-1] != "\n":
+                f.write("\n")
+    return f.name
+
+
+def diff(a: str, b: str, a_name: str, b_name: str) -> str:
+    """Return a unified diff string between strings `a` and `b`."""
+    a_lines = [line + "\n" for line in a.split("\n")]
+    b_lines = [line + "\n" for line in b.split("\n")]
+    return "".join(
+        difflib.unified_diff(a_lines, b_lines, fromfile=a_name, tofile=b_name, n=5)
+    )

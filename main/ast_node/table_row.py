25472508ffa1651864e4e9961398727ee0b85ff1
Duc-Minh Phan <alephvn@gmail.com>
2019-04-03 22:46:34 +0700

    Add dunder methods to get the length of a TableRow and get a TableCell by index

--- main/ast_node/table_row.py
+++ main/ast_node/table_row.py
@@ -15,3 +15,9 @@ class TableRow:
         yield self
 
         yield from self.cells
+
+    def __len__(self):
+        return len(self.cells)
+
+    def __getitem__(self, item):
+        return self.cells[item]

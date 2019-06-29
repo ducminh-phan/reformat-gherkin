21f6ac88132ab59311fb014aff3c91a8e6d8018d
Duc-Minh Phan <alephvn@gmail.com>
2019-06-29 11:38:16 +0700

    Add and refactor tests

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,6 +1,6 @@
 import pytest
 
-from reformat_gherkin.errors import InvalidInput
+from reformat_gherkin.errors import DeserializeError, InvalidInput
 from reformat_gherkin.parser import parse
 
 
@@ -13,3 +13,17 @@ def test_invalid_input(invalid_contents):
 def test_valid_input(valid_contents):
     for content in valid_contents():
         parse(content)
+
+
+def test_parse_with_exception(mocker, valid_contents):
+    exception_message = "exception message"
+    mocker.patch(
+        "reformat_gherkin.parser.converter.structure",
+        side_effect=Exception(exception_message),
+    )
+
+    for content in valid_contents():
+        with pytest.raises(DeserializeError) as exc_info:
+            parse(content)
+
+        assert exception_message in str(exc_info.value)

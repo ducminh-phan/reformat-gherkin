0f5ae8514e56f22a867b55845d89c06f4d08a885
Duc-Minh Phan <alephvn@gmail.com>
2019-06-29 11:09:56 +0700

    Add the functionalities to read config from file

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,6 +6,8 @@ from pathlib import Path
 import pytest
 from click.testing import CliRunner
 
+from reformat_gherkin.config import CONFIG_FILE
+
 
 @pytest.fixture()
 def valid_contents():
@@ -41,13 +43,29 @@ def invalid_contents():
 
 @pytest.fixture()
 def sources(request):
-    def construct_sources(contain_invalid=True):
+    def construct_sources(
+        contain_invalid=True,
+        with_config_file=False,
+        valid_config=True,
+        empty_config=False,
+    ):
         tmp_dir = Path(f"tmp{os.urandom(4).hex()}")
         shutil.copytree("tests/data", tmp_dir)
 
         base_dir = tmp_dir
         if not contain_invalid:
             base_dir = tmp_dir / "valid"
+
+        if with_config_file:
+            file_name = "valid"
+            if not valid_config:
+                file_name = "invalid"
+            if empty_config:
+                file_name = "empty"
+
+            shutil.copyfile(
+                "tests/config_files/{}.yaml".format(file_name), tmp_dir / CONFIG_FILE
+            )
 
         def fin():
             shutil.rmtree(tmp_dir)

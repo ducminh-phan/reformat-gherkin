d2e83ab9c12ca818db5013b2fce54acdef1d7451
Duc-Minh Phan <alephvn@gmail.com>
2019-12-20 20:55:58 +0700

    Escape pipe characters in table cells
    Fix #21

--- reformat_gherkin/ast_node/table_cell.py
+++ reformat_gherkin/ast_node/table_cell.py
@@ -1,7 +1,18 @@
+from attr import attrib
+
 from ._base import prepare
 from .location import LocationMixin
 
 
+def escape_table_cell_value(text: str) -> str:
+    """
+    Escape pipe characters `|` in a table cell's value. Since the pipe characters
+    are used to separate cells in a row, we need to replace them by `\\|`.
+    """
+
+    return text.replace("|", "\\|")
+
+
 @prepare
 class TableCell(LocationMixin):
-    value: str
+    value: str = attrib(converter=escape_table_cell_value)

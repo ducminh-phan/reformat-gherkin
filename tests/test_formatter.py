b6613e398c3bf586d72b7948423a1468fd860be4
Duc-Minh Phan <alephvn@gmail.com>
2020-01-12 18:43:15 +0700

    Make sure INDENT_LEVEL_MAP contains all necessary node types

--- tests/test_formatter.py
+++ tests/test_formatter.py
@@ -1,6 +1,23 @@
 from reformat_gherkin.ast_node import GherkinDocument
-from reformat_gherkin.formatter import LineGenerator
+from reformat_gherkin.formatter import INDENT_LEVEL_MAP, LineGenerator
 from reformat_gherkin.options import AlignmentMode
+
+
+def verify_indent_level_map():
+    """
+    Make sure that all node types with tags are included in the indent map.
+    """
+    import reformat_gherkin.ast_node as ast_node
+
+    node_types = [
+        attr
+        for attr in (getattr(ast_node, name) for name in dir(ast_node))
+        if isinstance(attr, type)
+    ]
+
+    for node_type in node_types:
+        if hasattr(node_type, "tags"):
+            assert node_type in INDENT_LEVEL_MAP
 
 
 def format_ast(ast, alignment_mode=AlignmentMode.LEFT):

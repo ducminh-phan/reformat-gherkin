9fe50d8376b38188a60b8ec7f01c1926de392568
Yuri Pieters <yuri.pieters@anaplan.com>
2021-03-17 16:29:27 +0000

    Update cli tests to check stdout is empty
    This promise is made in the README, but not tested.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -4,24 +4,28 @@ from reformat_gherkin.cli import main
 def test_cli_success(runner, sources):
     result = runner.invoke(main, [*sources(contain_invalid=False)])
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 0
 
 
 def test_cli_check(runner, sources):
     result = runner.invoke(main, [*sources(contain_invalid=False), "--check"])
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 1
 
 
 def test_cli_failed(runner, sources):
     result = runner.invoke(main, [*sources(), "--check"])
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 123
 
 
 def test_cli_empty_sources(runner):
     result = runner.invoke(main)
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 0
 
 
@@ -30,8 +34,9 @@ def test_cli_check_with_valid_config(runner, sources):
         main, [*sources(contain_invalid=False, with_config_file=True)]
     )
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 1
-    assert result.stdout.startswith("Using configuration from")
+    assert result.stderr.startswith("Using configuration from")
 
 
 def test_cli_check_with_invalid_config(runner, sources):
@@ -40,8 +45,9 @@ def test_cli_check_with_invalid_config(runner, sources):
         [*sources(contain_invalid=False, with_config_file=True, valid_config=False)],
     )
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 1
-    assert result.stdout.startswith("Error: Could not open file")
+    assert result.stderr.startswith("Error: Could not open file")
 
 
 def test_cli_check_with_empty_config(runner, sources):
@@ -50,4 +56,5 @@ def test_cli_check_with_empty_config(runner, sources):
         [*sources(contain_invalid=False, with_config_file=True, empty_config=True)],
     )
 
+    assert len(result.stdout) == 0
     assert result.exit_code == 0

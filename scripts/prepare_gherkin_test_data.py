13947c2cddb753b1ce1ddfb2629a7a4c33da4f4b
Duc-Minh Phan <alephvn@gmail.com>
2025-03-16 10:21:51 +0700

    Update gherkin-official to 30.0.4 and gherkin test data

--- scripts/prepare_gherkin_test_data.py
+++ scripts/prepare_gherkin_test_data.py
@@ -5,6 +5,8 @@ import tempfile
 from collections.abc import Iterable
 from pathlib import Path
 
+import tomli
+
 
 GHERKIN_TEST_DATA_DIR = Path("tests/gherkin_test_data")
 
@@ -21,19 +23,30 @@ def compute_hash(paths: Iterable[Path]) -> str:
     return h.hexdigest()
 
 
+def get_gherkin_tag() -> str:
+    with open("pyproject.toml", "rb") as f:
+        pyproject_data = tomli.load(f)
+
+    version = pyproject_data["tool"]["poetry"]["dependencies"]["gherkin-official"]
+    return f"v{version}"
+
+
 def prepare_data():
+    gherkin_tag = get_gherkin_tag()
+
     with tempfile.TemporaryDirectory() as tmp_dir:
         subprocess.run(
             [
                 "git",
                 "clone",
                 "--depth=1",
-                "https://github.com/cucumber/common",
+                f"--branch={gherkin_tag}",
+                "https://github.com/cucumber/gherkin",
                 tmp_dir,
             ],
         )
 
-        origin_test_data_dir = Path(tmp_dir) / "gherkin" / "testdata" / "good"
+        origin_test_data_dir = Path(tmp_dir) / "testdata" / "good"
         origin_hash = compute_hash(get_test_data_files(origin_test_data_dir))
         current_hash = compute_hash(get_test_data_files(GHERKIN_TEST_DATA_DIR))
 
